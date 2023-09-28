b9e3a9c7e09fa311eaf1398fda92509a41204d75
Aaron Powell <me@aaron-powell.com>
2023-09-28 00:27:49 +0000

    logos

--- src/GenerateAndChill.Client/src/main.tsx
+++ src/GenerateAndChill.Client/src/main.tsx
@@ -6,7 +6,7 @@ import { Index } from "./pages/Index";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
-      <div className="absolute inset-0 bg-[url(/azure-logo.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
+      <div className="absolute inset-0 bg-[url(/merged.png)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
       <Index />
     </div>
   </React.StrictMode>
